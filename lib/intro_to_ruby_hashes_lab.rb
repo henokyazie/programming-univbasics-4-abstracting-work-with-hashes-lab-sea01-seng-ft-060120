4bd04899240feb5351bf6a6610f0488b468f9de3
brave-ping-4201 <h_yazie@yahoo.com>
2020-05-14 03:48:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  hash = {:name => 'Grace Hopper', :id => 5, "age" =>39}
+  hash = {key => value}
 end
 
 def read_from_hash(hash, key)
